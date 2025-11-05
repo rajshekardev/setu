ea09433784b3315fe16d979c83d4766b69ea53b2
rajshekar <rajshekar.work@gmail.com>
2025-11-06 03:39:29 +0530

    init + Bun setup + vercel?

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
+import { message } from "./test";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
+const app = new Elysia().get("/", () => message);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
