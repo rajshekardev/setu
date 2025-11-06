fee60db4b3bae72f142e069767a410917f6a8d28
rajshekar <rajshekar.work@gmail.com>
2025-11-06 06:28:37 +0530

    cant remeber what i changed. it works now i guess

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,64 @@
-import { Elysia, Context } from "elysia";
-import { message } from "~/test";
-import { auth } from "~/lib/auth";
+import { Elysia } from "elysia";
+import { auth } from "@/lib/auth";
+import { RPCHandler } from "@orpc/server/fetch";
+import { appRouter } from "./api/routers";
+import { onError } from "@orpc/server";
+import { OpenAPIHandler } from "@orpc/openapi/fetch";
+import { OpenAPIReferencePlugin } from "@orpc/openapi/plugins";
+import { ZodToJsonSchemaConverter } from "@orpc/zod";
+import { cors } from "@elysiajs/cors";
+import { createContext } from "./api/context";
 
-const betterAuthView = (context: Context) => {
-  const BETTER_AUTH_ACCEPT_METHODS = ["POST", "GET"];
-  if (BETTER_AUTH_ACCEPT_METHODS.includes(context.request.method)) {
-    return auth.handler(context.request);
-  } else {
-    context.status("Method Not Allowed");
-  }
-};
-const ver = process.versions?.bun ?? "unknown";
+const rpcHandler = new RPCHandler(appRouter, {
+  interceptors: [
+    onError((error) => {
+      console.error(error);
+    }),
+  ],
+});
+const apiHandler = new OpenAPIHandler(appRouter, {
+  plugins: [
+    new OpenAPIReferencePlugin({
+      schemaConverters: [new ZodToJsonSchemaConverter()],
+    }),
+  ],
+  interceptors: [
+    onError((error) => {
+      console.error(error);
+    }),
+  ],
+});
+
 const app = new Elysia()
-  .all("/api/auth/*", betterAuthView)
-  .get("/", () => message)
-  .get("/version", () => ver);
+  .use(
+    cors({
+      origin: process.env.CORS_ORIGIN || "",
+      methods: ["GET", "POST", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    }),
+  )
+  .all("/api/auth/*", async (context) => {
+    const { request, status } = context;
+    if (["POST", "GET"].includes(request.method)) {
+      return auth.handler(request);
+    }
+    return status(405);
+  })
+  .all("/rpc*", async (context) => {
+    const { response } = await rpcHandler.handle(context.request, {
+      prefix: "/rpc",
+      context: await createContext({ context }),
+    });
+    return response ?? new Response("Not Found", { status: 404 });
+  })
+  .all("/api*", async (context) => {
+    const { response } = await apiHandler.handle(context.request, {
+      prefix: "/api-reference",
+      context: await createContext({ context }),
+    });
+    return response ?? new Response("Not Found", { status: 404 });
+  })
+  .get("/", () => "OK");
 
 export default app;
